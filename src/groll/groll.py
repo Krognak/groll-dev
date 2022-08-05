791979b6b2b2e0d19b0e7246ad16afb6f2eb571c
Krognak <69942446+Krognak@users.noreply.github.com>
2022-08-05 23:40:40 +0100

    simple capture of args from stdin

--- src/groll/groll.py
+++ src/groll/groll.py
@@ -1,10 +1,18 @@
 """A helpful, dice rolling goblin for the command line!"""
+
+import sys
+
 from groll import __version__
 
 
-def cli():
+def cli(args=None):
     """entrypoint for CLI"""
-    print(f"groll v{__version__} - {__doc__}")
+    if not args:
+        args = sys.argv[1:]
+    if "-v" in args or "--version" in args:
+        print(f"groll v{__version__} - {__doc__}")
+    else:
+        print(f"args supplied = {args}")
 
 
 if __name__ == "__main__":
