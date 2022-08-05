0b6259c6c466453b3bab01aa4f5da8be0be2f7fb
Krognak <69942446+Krognak@users.noreply.github.com>
2022-08-05 23:46:22 +0100

    check to see if flake8 warns about E203

--- src/groll/groll.py
+++ src/groll/groll.py
@@ -8,7 +8,7 @@ from groll import __version__
 def cli(args=None):
     """entrypoint for CLI"""
     if not args:
-        args = sys.argv[1:]
+        args = sys.argv[0 + 1 : :]
     if "-v" in args or "--version" in args:
         print(f"groll v{__version__} - {__doc__}")
     else:
