5061ee79c21a56d96935090e422924b0b7ac6f32
Krognak <69942446+Krognak@users.noreply.github.com>
2022-08-06 22:52:47 +0100

    --version option

--- src/groll/groll.py
+++ src/groll/groll.py
@@ -1,78 +1,13 @@
 """A helpful, dice rolling goblin for the command line!"""
 
-import operator
-import random
-import re
 import sys
 
 import click
 
 from . import __version__
 
-DICE_SYNTAX = r"\d*d\d+"
 
-OPERATORS = {
-    "+": operator.add,
-    "-": operator.sub,
-    "*": operator.mul,
-    "/": operator.floordiv,
-}
-
-
-def roll(match):
-    """roll dice strings of the form [n]d[s]"""
-    num, sides = match.group(0).split("d")
-    if not num:
-        num = 1
-    else:
-        num = int(num)
-    sides = int(sides)
-    return sum((random.randint(1, sides) for _ in range(num)))
-
-
-def str_to_int(args):
-    """sub string representation of int with int"""
-    subbed = []
-    for arg in args:
-        if arg:
-            match = re.match(r"\d+", arg)
-            if match:
-                subbed.append(int(match.group(0)))
-            else:
-                subbed.append(arg)
-    return subbed
-
-
-def parse(dice):
-    """parse dice input tuple into tuple of integers and operator strings"""
-    if not dice:
-        dstring = "1d20"
-    else:
-        dstring = " ".join(dice)
-    dstring = re.sub(DICE_SYNTAX, roll, dstring)
-    for strop in OPERATORS:
-        pattern = "".join(("\\", strop))
-        dstring = re.sub(pattern, f" {strop} ", dstring)
-        dstring = re.sub(" +", " ", dstring)
-    dstring = dstring.split()
-    dstring = str_to_int(dstring)
-    return dstring
-
-
-def eval_maths(math_list):
-    """evaluate maths safely from list"""
-    if len(math_list) == 1:
-        result = math_list[0]
-    else:
-        result, _operator, operand, *rest = math_list
-        while rest:
-            result = OPERATORS[_operator](result, operand)
-            _operator, operand, *rest = rest
-        result = OPERATORS[_operator](result, operand)
-    return result
-
-
-@click.command()
+@click.command(no_args_is_help=True)
 @click.option(
     "--version",
     is_flag=True,
@@ -85,9 +20,8 @@ def cli(dice, **kwargs):
     if kwargs["version"]:
         click.echo(f"groll v{__version__} - {__doc__}")
         sys.exit()
-    parsed_string = parse(dice)
-    result = eval_maths(parsed_string)
-    click.echo(result)
+    else:
+        click.echo(f"dice = {dice}")
 
 
 if __name__ == "__main__":
--- tests/test_groll.py
+++ tests/test_groll.py
@@ -1,16 +1,9 @@
+import tomli
+
 from src.groll import groll
 
 
 def test_version():
-    assert groll.__version__ == "0.0.1"
-
-
-def test_parser():
-    assert groll.parse(["4", "+9"]) == [4, "+", 9]
-
-
-def test_math_eval():
-    assert groll.eval_maths([1, "+", 2]) == 3
-    assert groll.eval_maths([1, "-", 3]) == -2
-    assert groll.eval_maths([4, "+", 7, "-", 8]) == 3
-    assert groll.eval_maths([8, "/", 2]) == 4
+    with open("pyproject.toml", "rb") as f:
+        metadata = tomli.load(f)
+    assert groll.__version__ == metadata["tool"]["poetry"]["version"]
