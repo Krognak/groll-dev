5f9a8455c238aa5e2a11ca90df70d8954c95ba05
Krognak <69942446+Krognak@users.noreply.github.com>
2022-08-07 00:55:53 +0100

    dice and related methods

--- src/groll/groll.py
+++ src/groll/groll.py
@@ -1,10 +1,43 @@
 """A helpful, dice rolling goblin for the command line!"""
 
+from __future__ import annotations
+
+import random
 import sys
+from dataclasses import dataclass
+from typing import Generator, List, Tuple, Union
 
 import click
 
 from . import __version__
+
+
+@dataclass()
+class Dice:
+    """Dice dataclass, defaults to a single die"""
+
+    sides: int
+    number: int = 1
+
+
+def get_dice(dice_string: str) -> Dice:
+    """Create Dice from string"""
+    number, sides = dice_string.split("d")
+    if number:
+        dice = Dice(int(sides), int(number))
+    else:
+        dice = Dice(int(sides))
+    return dice
+
+
+def roll(dice: Dice) -> Generator[int, None, None]:
+    """Generator containing random dice rolls"""
+    return (random.randint(1, dice.sides) for _ in range(dice.number))
+
+
+def parse(args: Tuple[str]) -> List[Union[str, int]]:
+    """Substitutes dice rolls and splits 'sticky' operators."""
+    return list(args)
 
 
 @click.command(no_args_is_help=True)
@@ -14,14 +47,14 @@ from . import __version__
     is_eager=True,
     help="Display version infomation and exit.",
 )
-@click.argument("dice", nargs=-1)
-def cli(dice, **kwargs):
+@click.argument("args", nargs=-1)
+def cli(args, **kwargs):
     """A helpful, dice rolling goblin for the command line!"""
     if kwargs["version"]:
         click.echo(f"groll v{__version__} - {__doc__}")
         sys.exit()
     else:
-        click.echo(f"dice = {dice}")
+        parse(args)
 
 
 if __name__ == "__main__":
