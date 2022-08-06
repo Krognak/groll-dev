64544b8296dfcdc34df744e489a7df3926668948
Krognak <69942446+Krognak@users.noreply.github.com>
2022-08-06 17:07:09 +0100

    parse dice from string and roll them

--- src/groll/groll.py
+++ src/groll/groll.py
@@ -1,15 +1,35 @@
 """A helpful, dice rolling goblin for the command line!"""
 
+import random
+import re
 import sys
 
 import click
 
 from . import __version__
 
+DICE_SYNTAX = r"\d*d\d+"
+OPERATORS = r"\+|-|\*|\/"
 
-def parse(args):
+
+def roll(dstring):
+    """roll dice strings of the form [n]d[s]"""
+    num, sides = dstring.group(0).split("d")
+    if not num:
+        num = 1
+    else:
+        num = int(num)
+    sides = int(sides)
+    return str(sum((random.randint(1, sides) for _ in range(num))))
+
+
+def parse(dice):
     """parse dice list into integer values"""
-    click.echo(f"(g)rolling: {args}")
+    if not dice:
+        dstring = "1d20"
+    else:
+        dstring = " ".join(dice)
+    return re.sub(DICE_SYNTAX, roll, dstring)
 
 
 @click.command()
@@ -25,7 +45,8 @@ def cli(dice, **kwargs):
     if kwargs["version"]:
         click.echo(f"groll v{__version__} - {__doc__}")
         sys.exit()
-    parse(dice)
+    parsed_string = parse(dice)
+    click.echo(parsed_string)
 
 
 if __name__ == "__main__":
