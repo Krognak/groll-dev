c48c7d04ac94346b515bd3ae8283fd36064a6697
Krognak <69942446+Krognak@users.noreply.github.com>
2022-08-06 21:44:43 +0100

    parser tweaks

--- tests/test_groll.py
+++ tests/test_groll.py
@@ -1,5 +1,16 @@
-from src import groll
+from src.groll import groll
 
 
 def test_version():
     assert groll.__version__ == "0.0.1"
+
+
+def test_parser():
+    assert groll.parse(["4", "+9"]) == [4, "+", 9]
+
+
+def test_math_eval():
+    assert groll.eval_maths([1, "+", 2]) == 3
+    assert groll.eval_maths([1, "-", 3]) == -2
+    assert groll.eval_maths([4, "+", 7, "-", 8]) == 3
+    assert groll.eval_maths([8, "/", 2]) == 4
